04389dcb2fae016f94097d4ccad281551b05d3f7
crawford <freddy_crawford@yahoo.com>
2018-03-27 20:44:11 -0600

    creating a robofriends app using react

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+{
+	id:1,
+	name:'Janet Jackson',
+	username:'diva',
+	email:'janet.jackson@gmail.com'
+},
+{
+	id:2,
+	name:'Jermaine Dupree',
+	username:'jd',
+	email:'jd@sosodef.com'
+},
+{
+	id:3,
+	name:'Michael Jackson',
+	username:'king of pop',
+	email:'mjking@google.com'
+},
+{
+	id:4,
+	name:'Tito Jackson',
+	username:'nobody',
+	email:'justajackson@aol.com'
+},
+{
+	id:5,
+	name:'Ike Turner',
+	username:'tinashusband',
+	email:'whatslovegottodowithit@yahoo.com'
+}]
+
