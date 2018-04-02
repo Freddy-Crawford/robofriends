0ba75895661c5051b3cf62b9314557391dfa7209
crawford <freddy_crawford@yahoo.com>
2018-04-02 09:40:02 -0600

    finished robofriends app

--- src/robots.js
+++ src/robots.js
@@ -1,30 +1,30 @@
 export const robots = [
 {
-	id:1,
+	id: 1,
 	name:'Janet Jackson',
 	username:'diva',
 	email:'janet.jackson@gmail.com'
 },
 {
-	id:2,
+	id: 2,
 	name:'Jermaine Dupree',
 	username:'jd',
 	email:'jd@sosodef.com'
 },
 {
-	id:3,
+	id: 3,
 	name:'Michael Jackson',
 	username:'king of pop',
 	email:'mjking@google.com'
 },
 {
-	id:4,
+	id: 4,
 	name:'Tito Jackson',
 	username:'nobody',
 	email:'justajackson@aol.com'
 },
 {
-	id:5,
+	id: 5,
 	name:'Ike Turner',
 	username:'tinashusband',
 	email:'whatslovegottodowithit@yahoo.com'
